b80938f49d5f7bacf012d383f959363c2f5deca5
John Paul Naranjo <swade84@hotmail.com>
2025-08-13 16:45:55 -0700

    Add The Making of a Programmer code

--- CSPGPreLim2/CSPGPreLim2/Program.cs
+++ CSPGPreLim2/CSPGPreLim2/Program.cs
@@ -4,3 +4,11 @@ Console.WriteLine("Hello, World!");
 //What comes next?
 Console.WriteLine("What comes next?");
 
+//The Making of a Programmer 
+Console.WriteLine("The Making of a Programmer");
+Console.WriteLine("Programming is a super power");
+Console.WriteLine("I love programming");
+Console.WriteLine("Tik tok tik tok");
+Console.WriteLine("I kick ass.");
+
+//Consolas and Telim
