aef949dd8691a1a152366ca5286422b4cfc49a7b
John Paul Naranjo <swade84@hotmail.com>
2025-08-14 20:40:57 -0700

    Added Consolas and Telim

--- CSPGPreLim2/CSPGPreLim2/Program.cs
+++ CSPGPreLim2/CSPGPreLim2/Program.cs
@@ -1,14 +1,18 @@
-﻿//HP Tracker
-Console.WriteLine("Hello, World!");
-
-//What comes next?
-Console.WriteLine("What comes next?");
-
-//The Making of a Programmer 
-Console.WriteLine("The Making of a Programmer");
-Console.WriteLine("Programming is a super power");
-Console.WriteLine("I love programming");
-Console.WriteLine("Tik tok tik tok");
-Console.WriteLine("I kick ass.");
+﻿// //HP Tracker
+// Console.WriteLine("Hello, World!");
+//
+// //What comes next?
+// Console.WriteLine("What comes next?");
+//
+// //The Making of a Programmer 
+// Console.WriteLine("The Making of a Programmer");
+// Console.WriteLine("Programming is a super power");
+// Console.WriteLine("I love programming");
+// Console.WriteLine("Tik tok tik tok");
+// Console.WriteLine("I kick ass.");
 
 //Consolas and Telim
+Console.WriteLine("Bread is ready.");
+Console.WriteLine("Who is this bread for?");
+string breadFor = Console.ReadLine();
+Console.WriteLine($"Noted: {breadFor} got bread.");
