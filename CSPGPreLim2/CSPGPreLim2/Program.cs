96dfb127893edf2ddd4285c143100b04e8dded2c
John Paul Naranjo <swade84@hotmail.com>
2025-08-16 17:41:45 -0700

    Refactored the variables to be more descriptive and easy to read.

--- CSPGPreLim2/CSPGPreLim2/Program.cs
+++ CSPGPreLim2/CSPGPreLim2/Program.cs
@@ -19,9 +19,9 @@
 
 //The Things Namer 3000
 Console.WriteLine("What kind of thing are we talking about?");
-string a = Console.ReadLine();
+string nameOfTheThing = Console.ReadLine();
 Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
-string b = Console.ReadLine();
-string c = "of Doom";
-string d = "3000";
-Console.WriteLine("The "+ b + " " + a + " of " + c + " " + d );
+string DescriptionOfTheThing = Console.ReadLine();
+string doom = "Doom";
+string thingVersion = "3000";
+Console.WriteLine("The "+ DescriptionOfTheThing + " " + nameOfTheThing + " of " + doom + " " + thingVersion );
