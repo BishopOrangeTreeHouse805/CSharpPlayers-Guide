d0f1919d4d3dae6a33a401dff2c3549012f48186
John Paul Naranjo <swade84@hotmail.com>
2025-11-10 16:36:23 -0800

    Added The Variable Shop Challenge

--- CSPGPreLim2/CSPGPreLim2/Program.cs
+++ CSPGPreLim2/CSPGPreLim2/Program.cs
@@ -1,27 +1,64 @@
-﻿// //HP Tracker
-// Console.WriteLine("Hello, World!");
+﻿//  //HP Tracker
+//  Console.WriteLine("Hello, World!");
 //
-// //What comes next?
-// Console.WriteLine("What comes next?");
+//  //What comes next?
+//  Console.WriteLine("What comes next?");
 //
-// //The Making of a Programmer 
-// Console.WriteLine("The Making of a Programmer");
-// Console.WriteLine("Programming is a super power");
-// Console.WriteLine("I love programming");
-// Console.WriteLine("Tik tok tik tok");
-// Console.WriteLine("I kick ass.");
+//  //The Making of a Programmer 
+//  Console.WriteLine("The Making of a Programmer");
+//  Console.WriteLine("Programming is a super power");
+//  Console.WriteLine("I love programming");
+//  Console.WriteLine("Tik tok tik tok");
+//  Console.WriteLine("I kick ass.");
+//
+//  //Consolas and Telim
+//  Console.WriteLine("Bread is ready.");
+//  Console.WriteLine("Who is this bread for?");
+//  string breadFor = Console.ReadLine();
+//  Console.WriteLine($"Noted: {breadFor} got bread.");
+//
+// //The Things Namer 3000
+// Console.WriteLine("What kind of thing are we talking about?");
+// string nameOfTheThing = Console.ReadLine();
+// Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+// string DescriptionOfTheThing = Console.ReadLine();
+// string doom = "Doom";
+// string thingVersion = "3000";
+// Console.WriteLine("The "+ DescriptionOfTheThing + " " + nameOfTheThing + " of " + doom + " " + thingVersion );
 
-//Consolas and Telim
-// Console.WriteLine("Bread is ready.");
-// Console.WriteLine("Who is this bread for?");
-// string breadFor = Console.ReadLine();
-// Console.WriteLine($"Noted: {breadFor} got bread.");
 
-//The Things Namer 3000
-Console.WriteLine("What kind of thing are we talking about?");
-string nameOfTheThing = Console.ReadLine();
-Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
-string DescriptionOfTheThing = Console.ReadLine();
-string doom = "Doom";
-string thingVersion = "3000";
-Console.WriteLine("The "+ DescriptionOfTheThing + " " + nameOfTheThing + " of " + doom + " " + thingVersion );
+
+//The Variable Shop 
+char letter = 'J';
+string term = "Santa Barbara";
+bool isDenied = false;
+float fraction = 1.7f;
+double ratio = 2.334556;
+decimal money = 1000.00m;
+int number = 3;
+uint digit = 3;
+long distance = 4500000;
+ulong distance2 = 4500001;
+short balance = 30;
+ushort debt = 45;
+byte age = 25;
+sbyte acceleration = -10;
+
+
+Console.WriteLine(letter);
+Console.WriteLine(term);
+Console.WriteLine(isDenied);
+Console.WriteLine(fraction);
+Console.WriteLine(ratio);
+Console.WriteLine(money);
+Console.WriteLine(number);
+Console.WriteLine(digit);
+Console.WriteLine(distance);
+Console.WriteLine(distance2);
+Console.WriteLine(balance);
+Console.WriteLine(debt);
+Console.WriteLine(age);
+Console.WriteLine(acceleration);
+
+
+//The Variable Shop Returns.
