8a77de38d9a7d91aaff632a153e5ff697fabbf67
John Paul Naranjo <swade84@hotmail.com>
2025-11-12 14:56:36 -0800

    Added 'The Triangle Farmer' Challenge

--- CSPGPreLim2/CSPGPreLim2/Program.cs
+++ CSPGPreLim2/CSPGPreLim2/Program.cs
@@ -1,63 +1,73 @@
-﻿//  //HP Tracker
-//  Console.WriteLine("Hello, World!");
+﻿// //  //HP Tracker
+// //  Console.WriteLine("Hello, World!");
+// //
+// //  //What comes next?
+// //  Console.WriteLine("What comes next?");
+// //
+// //  //The Making of a Programmer 
+// //  Console.WriteLine("The Making of a Programmer");
+// //  Console.WriteLine("Programming is a super power");
+// //  Console.WriteLine("I love programming");
+// //  Console.WriteLine("Tik tok tik tok");
+// //  Console.WriteLine("I kick ass.");
+// //
+// //  //Consolas and Telim
+// //  Console.WriteLine("Bread is ready.");
+// //  Console.WriteLine("Who is this bread for?");
+// //  string breadFor = Console.ReadLine();
+// //  Console.WriteLine($"Noted: {breadFor} got bread.");
+// //
+// // //The Things Namer 3000
+// // Console.WriteLine("What kind of thing are we talking about?");
+// // string nameOfTheThing = Console.ReadLine();
+// // Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+// // string DescriptionOfTheThing = Console.ReadLine();
+// // string doom = "Doom";
+// // string thingVersion = "3000";
+// // Console.WriteLine("The "+ DescriptionOfTheThing + " " + nameOfTheThing + " of " + doom + " " + thingVersion );
 //
-//  //What comes next?
-//  Console.WriteLine("What comes next?");
 //
-//  //The Making of a Programmer 
-//  Console.WriteLine("The Making of a Programmer");
-//  Console.WriteLine("Programming is a super power");
-//  Console.WriteLine("I love programming");
-//  Console.WriteLine("Tik tok tik tok");
-//  Console.WriteLine("I kick ass.");
 //
-//  //Consolas and Telim
-//  Console.WriteLine("Bread is ready.");
-//  Console.WriteLine("Who is this bread for?");
-//  string breadFor = Console.ReadLine();
-//  Console.WriteLine($"Noted: {breadFor} got bread.");
+// //The Variable Shop and The Variable Shop Returns.
+// char letter = 't';
+// string term = "San Clemente";
+// bool isDenied = true;
+// float fraction = 1.70f;
+// double ratio = 2.4556;
+// decimal money = 3000.00m;
+// int number = -50;
+// uint digit = 6;
+// long distance = 5000000;
+// ulong distance2 = 5000001;
+// short balance = 31;
+// ushort debt = 46;
+// byte age = 5;
+// sbyte acceleration = -1;
 //
-// //The Things Namer 3000
-// Console.WriteLine("What kind of thing are we talking about?");
-// string nameOfTheThing = Console.ReadLine();
-// Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
-// string DescriptionOfTheThing = Console.ReadLine();
-// string doom = "Doom";
-// string thingVersion = "3000";
-// Console.WriteLine("The "+ DescriptionOfTheThing + " " + nameOfTheThing + " of " + doom + " " + thingVersion );
+//
+// Console.WriteLine(letter);
+// Console.WriteLine(term);
+// Console.WriteLine(isDenied);
+// Console.WriteLine(fraction);
+// Console.WriteLine(ratio);
+// Console.WriteLine(money);
+// Console.WriteLine(number);
+// Console.WriteLine(digit);
+// Console.WriteLine(distance);
+// Console.WriteLine(distance2);
+// Console.WriteLine(balance);
+// Console.WriteLine(debt);
+// Console.WriteLine(age);
+// Console.WriteLine(acceleration);
+//
+//
+//The Triangle Farmer
+Console.WriteLine("What is the base of the triangle?");
+float baseOfTriangle = float.Parse(Console.ReadLine());
+Console.WriteLine("What is the height of the triangle?");
+float heightOfTriangle = float.Parse(Console.ReadLine());
 
 
+float areaOfTriangle = baseOfTriangle * heightOfTriangle / 2;
 
-//The Variable Shop and The Variable Shop Returns.
-char letter = 't';
-string term = "San Clemente";
-bool isDenied = true;
-float fraction = 1.70f;
-double ratio = 2.4556;
-decimal money = 3000.00m;
-int number = -50;
-uint digit = 6;
-long distance = 5000000;
-ulong distance2 = 5000001;
-short balance = 31;
-ushort debt = 46;
-byte age = 5;
-sbyte acceleration = -1;
-
-
-Console.WriteLine(letter);
-Console.WriteLine(term);
-Console.WriteLine(isDenied);
-Console.WriteLine(fraction);
-Console.WriteLine(ratio);
-Console.WriteLine(money);
-Console.WriteLine(number);
-Console.WriteLine(digit);
-Console.WriteLine(distance);
-Console.WriteLine(distance2);
-Console.WriteLine(balance);
-Console.WriteLine(debt);
-Console.WriteLine(age);
-Console.WriteLine(acceleration);
-
-
+Console.WriteLine($"The area of the triangle with a base of {baseOfTriangle} and height of {heightOfTriangle} is {areaOfTriangle}");
