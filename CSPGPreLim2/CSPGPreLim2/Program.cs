f2bf2116af5f3f39787c4312a0b698faaca9fb6f
John Paul Naranjo <swade84@hotmail.com>
2025-11-16 20:00:27 -0800

    Added 'Challenge: The Watchtower'

--- CSPGPreLim2/CSPGPreLim2/Program.cs
+++ CSPGPreLim2/CSPGPreLim2/Program.cs
@@ -117,7 +117,7 @@
 //Challenge: The Defense of Consolas.
 
 using System.Data;
-
+//Challenge: The Defense of Consolas.
 // Console.WriteLine("Enter the row: ");
 // int row = int.Parse(Console.ReadLine());
 //
@@ -134,11 +134,44 @@ using System.Data;
 
 
 //Challenge: Repairing the Clock Tower
-Console.WriteLine("Enter the clock time number: ");
-int numericInput = int.Parse(Console.ReadLine());
+// Console.WriteLine("Enter the clock time number: ");
+// int numericInput = int.Parse(Console.ReadLine());
+//
+// string clockSound = numericInput % 2 == 0
+//     ? "Tick"
+//     : "Tock";
+//     
+// Console.WriteLine(clockSound);
 
-string clockSound = numericInput % 2 == 0
-    ? "Tick"
-    : "Tock";
-    
-Console.WriteLine(clockSound);
+//Challenge: Watchtower
+Console.WriteLine("Enter the x - coordinate");
+int x = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the y - coordinate");
+int y = int.Parse(Console.ReadLine());
+
+if (x == 0 && y == 0) Console.WriteLine("The enemy is here!");
+
+if (x > 0 && y > 0)   EnemyCurrentLocation("northeast"); 
+
+if (x > 0 && y < 0)   EnemyCurrentLocation("southeast"); 
+
+if (x == 0 && y > 0 ) EnemyCurrentLocation("north");   
+
+if (x == 0 && y < 0 ) EnemyCurrentLocation("south");
+
+if (x < 0 && y > 0)   EnemyCurrentLocation("northwest");
+
+if (x < 0 && y < 0)   EnemyCurrentLocation("southwest"); 
+
+if (x < 0 && y == 0)  EnemyCurrentLocation("west"); 
+
+if (x > 0 && y == 0)  EnemyCurrentLocation("east");
+
+
+
+
+void EnemyCurrentLocation(string direction)
+{
+    Console.WriteLine($"The enemy is to the {direction}!");
+};
