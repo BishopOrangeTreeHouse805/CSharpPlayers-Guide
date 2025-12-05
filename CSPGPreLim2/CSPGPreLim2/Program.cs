5cf9f531c9f8d2972c4d60dec457ea13e5e4fcf7
John Paul Naranjo <swade84@hotmail.com>
2025-12-05 09:48:20 -0800

    Added Buying Inventory Challenge

--- CSPGPreLim2/CSPGPreLim2/Program.cs
+++ CSPGPreLim2/CSPGPreLim2/Program.cs
@@ -178,19 +178,55 @@
 
 
 //Challenge: Buying Inventory
-Console.WriteLine($@"The following items are available: 
+Console.WriteLine($@"The following items are available:
 1 - Rope
 2 - Torches
 3 - Climbing Equipment
 4 - Clean Water
 5 - Machete
 6 - Canoe
-7 - Food Supplies
-");
+7 - Food Supplies");
 
 Console.Write("What number do you want to see the price of? ");
-int itemSelectionNumber = int.Parse(Console.ReadLine());
+string? input = Console.ReadLine();
 
+if (!int.TryParse(input, out int itemChoiceNumber))
+{
+    Console.WriteLine("Invalid input. Please enter a number.");
+    return;
+}
+
+string? item = itemChoiceNumber switch
+{
+    1 => "Rope",
+    2 => "Torches",
+    3 => "Climbing Equipment",
+    4 => "Clean Water",
+    5 => "Machete",
+    6 => "Canoe",
+    7 => "Food Supplies",
+    _ => null
+};
+
+if (item == null)
+{
+    Console.WriteLine("Invalid input. Please enter a number between 1 and 7.");
+    return;
+}
+
+byte itemGoldPrice = item switch
+{
+    "Rope" => 10,
+    "Torches" => 15,
+    "Climbing Equipment" => 25,
+    "Clean Water" => 1,
+    "Machete" => 20,
+    "Canoe" => 200,
+    "Food Supplies" => 1,
+    _ => throw new Exception("Invalid Action. Please contact support.")
+};
+
+Console.WriteLine($"{item} costs {itemGoldPrice} gold.");
 //Challenge: Discounted Inventory
 
 
