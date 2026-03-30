7b769dc5cd5e55e7764b6891242580bdd5ff837e
John Paul Naranjo <swade84@hotmail.com>
2026-03-30 11:39:36 -0700

    Redo AskForNumber method since it didn't handle the case of non-numeric input and added AskForNumberInRange method using the other method as component.

---
+++ TakingANumber/TakingANumber/Program.cs
@@ -0,0 +1,31 @@
+﻿//Taking a number
+
+int AskForNumber(string text)
+{
+    while (true)
+    {
+        Console.Write(text);
+        if (int.TryParse(Console.ReadLine(), out int number))
+            return number;
+        
+        Console.WriteLine("Please enter a valid number.");
+    }
+}
+
+
+
+int AskForNumberInRange(string text, int min, int max)
+{
+    while (true)
+    {
+        int number = AskForNumber(text);
+        
+        if (number > min && number < max)
+             {
+                 return number;
+             }
+        Console.WriteLine($"Please enter a number between {min} and {max}: ");
+    }
+    
+}
+
