389c56b55b4f80985ba0182b188ad8ef9fee29b1
John Paul Naranjo <swade84@hotmail.com>
2026-03-29 22:38:39 -0700

    added the Laws of Freach exercises

---
+++ TheLawsOfFreach/TheLawsOfFreach/Program.cs
@@ -0,0 +1,23 @@
+﻿//The Laws of Freach
+int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+    
+int currentSmallest = int.MaxValue; // Start higher than anything in the array.
+
+foreach (var arrayItem in array)
+{
+    if (arrayItem < currentSmallest )
+        currentSmallest = arrayItem;
+}
+
+Console.WriteLine($"The smallest number in the array is {currentSmallest}");
+
+Console.WriteLine("------------------------------------------------------------");
+
+int total = 0;
+
+foreach (var arrayItem in array)
+{
+    total += arrayItem;
+}
+float average = (float)total / array.Length;
+Console.WriteLine($"The average of thi array is {average}");
