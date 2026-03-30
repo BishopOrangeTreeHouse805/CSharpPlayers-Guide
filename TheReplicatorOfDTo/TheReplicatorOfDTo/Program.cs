57dfec770960ff5c15fe011745b33d883c5074b9
John Paul Naranjo <swade84@hotmail.com>
2026-03-29 21:42:30 -0700

    Added The ReplicatorOfD'To exercise

---
+++ TheReplicatorOfDTo/TheReplicatorOfDTo/Program.cs
@@ -0,0 +1,26 @@
+﻿//The Replicator of D'To
+
+int[] numberArray = new int[5];
+
+
+for (int i = 0; i < numberArray.Length; i++)
+{
+    int userInput = int.Parse(Console.ReadLine());
+    numberArray[i] = userInput;
+}
+
+
+int[] duplicateArray = new int[5];
+
+for (int i = 0; i < duplicateArray.Length; i++)
+{
+    duplicateArray[i] = numberArray[i];
+}
+
+Console.WriteLine("\n Index | Original | Duplicate");
+Console.WriteLine(" ------|----------|----------");
+
+for (int i = 0; i < numberArray.Length; i++)
+{
+    Console.WriteLine($"  [{i}]  |    {numberArray[i],-6}|    {duplicateArray[i]}");
+}
