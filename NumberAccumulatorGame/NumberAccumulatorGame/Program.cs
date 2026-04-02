dca5f7b658670ab7593cee7baede009d39ee1227
John Paul Naranjo <swade84@hotmail.com>
2026-04-01 19:45:00 -0700

    Added Challenge: Number Accumulator Game - Claude

---
+++ NumberAccumulatorGame/NumberAccumulatorGame/Program.cs
@@ -0,0 +1,9 @@
+﻿//Challenge: Number Accumulator Game
+Console.WriteLine("How many rounds do you wish play this game? (max is 10)");
+int userInput = int.Parse(Console.ReadLine());
+
+if (userInput > 10 && userInput <= 1)
+{
+    Console.WriteLine("Please enter a number between 1 and 10 (max is 10) and press enter to continue.");
+}
+
